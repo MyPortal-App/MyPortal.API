75275c9092781cadab99d917747de3b0b10768d6
rickymster <rickymster@gmail.com>
2020-10-28 18:48:56 +0200

    Update - Enable Authentication

--- MyPortal.API/Controllers/UserManagerController.cs
+++ MyPortal.API/Controllers/UserManagerController.cs
@@ -64,7 +64,7 @@ namespace MyPortal.API.Controllers
             return Ok(user);
         }
 
-        //[Authorize]
+        [Authorize]
         [Route("SaveUserDetails")]
         [HttpPost]
         public async Task<IActionResult> SaveUserDetails(Entity.DbEntities.User user)
